a3598b73b7908040549eb01eb54470040cfe81d4
hotpaprika <97948972+hotpaprika@users.noreply.github.com>
2023-03-28 08:47:14 +0300

    pievienoju funkcijas
    pievienoju funkcijas

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const plusins = document.querySelector('.js-plus');
 const inputins = document.querySelector('.js-input')
-console.log(plusins)
-console.log(inputins)
+const add = (cipars1, cipars2) =>{
+return cipars1 + cipars2
+}
+const subtract = (cipars1, cipars2) =>{
+return cipars1 - cipars2
+}
+const multiply = (cipars1, cipars2) =>{
+    return cipars1 + cipars2
+}
+const divide = (cipars1, cipars2) =>{
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => {
+    console.log("mau")
+})
+
+
